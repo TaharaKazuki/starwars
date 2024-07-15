442f0696f4cbfba3e96542614275b876d3a2fc0f
Tahara Kazuki <>
2024-07-15 11:12:05 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { useState } from 'react';
 
 import { cn } from '@/lib/utils';
@@ -14,17 +15,21 @@ export default function Home() {
   return (
     <main className={'flex min-h-screen flex-col gap-16 bg-primary p-24'}>
       <div className="flex flex-col text-white/45">
-        <h1 className="text-3xl font-semibold">Lightsaber</h1>
+        <h1 className="text-3xl font-semibold uppercase">Lightsaber</h1>
       </div>
-      <div className="flex flex-col items-center gap-2">
-        <h2 className="text-left text-white">ジェダイ</h2>
+      <div className="flex flex-col items-center gap-16">
         <div className="relative flex w-[700px] items-center">
-          <button
-            onClick={toggleExpand}
-            className="absolute -left-12 z-10 bg-black text-white/60"
-          >
-            セーバー
-          </button>
+          <div onClick={toggleExpand} className="absolute left-[-45px]">
+            <Image
+              src="/assets/Lightsaber.png"
+              alt="Lightsaber"
+              priority
+              quality={100}
+              className="object-contain"
+              width={100}
+              height={40}
+            />
+          </div>
           <div
             className={cn(
               'p-1 rounded-2xl neon-jedi bg-white',
@@ -33,11 +38,26 @@ export default function Home() {
             style={{ width: expanded ? '100%' : '0%' }}
           />
         </div>
-        {/* <div className="w-[700px]">
+        <div className="relative flex w-[700px] items-center">
+          <div onClick={toggleExpand} className="absolute left-[-45px]">
+            <Image
+              src="/assets/Lightsaber.png"
+              alt="Lightsaber"
+              priority
+              quality={100}
+              className="object-contain"
+              width={100}
+              height={40}
+            />
+          </div>
           <div
-            className={cn(`p-1 rounded-2xl neon-sith bg-white animate-expand`)}
+            className={cn(
+              'p-1 rounded-2xl neon-sith bg-white',
+              expanded ? 'animate-expand' : 'animate-shrink'
+            )}
+            style={{ width: expanded ? '100%' : '0%' }}
           />
-        </div> */}
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,8 +52,8 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        expand: 'expand 1s ease-in-out forwards',
-        shrink: 'shrink 1s ease-in-out forwards',
+        expand: 'expand 0.5s ease-in-out forwards',
+        shrink: 'shrink 0.5s ease-in-out forwards',
       },
     },
   },
