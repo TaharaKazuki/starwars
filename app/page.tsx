a3b7ce45c4e74b7b55bf542f5e2cb3c6d205ac8b
Tahara Kazuki <>
2024-07-15 11:33:52 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,19 @@ import { LightSaber } from '@/components/Lightsaber';
 export default function Home() {
   return (
     <main className={'flex min-h-screen flex-col gap-16 p-24'}>
-      <div className="flex flex-col text-white/45">
-        <h1 className="text-center text-3xl font-semibold uppercase">
+      <div className="flex flex-col">
+        <h1 className="text-outline text-center text-3xl font-semibold uppercase text-black/45">
           Lightsaber
         </h1>
       </div>
       <div className="flex flex-col items-center gap-16">
-        <h2 className="text-2xl font-semibold text-neon-jedi">Jedi</h2>
+        <h2 className="text-outline text-2xl font-semibold uppercase text-neon-jedi">
+          Jedi
+        </h2>
         <LightSaber type="jedi" imageUrl="/assets/Lightsaber.png" />
-        <h2 className="text-2xl font-semibold text-neon-sith">Sith</h2>
+        <h2 className="text-outline text-2xl font-semibold uppercase text-neon-sith">
+          Sith
+        </h2>
         <LightSaber type="sith" imageUrl="/assets/Lightsaber.png" />
       </div>
     </main>
