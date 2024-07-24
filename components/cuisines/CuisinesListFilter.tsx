8069c4261072ba0c7c5d9850431b77f89469a542
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-24 19:44:30 +0900

    update

--- components/cuisines/CuisinesListFilter.tsx
+++ components/cuisines/CuisinesListFilter.tsx
@@ -34,7 +34,7 @@ export const CuisinesListFilter = ({ onChange }: CuisinesListFilterProps) => {
         type="text"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search products"
+        placeholder="Search cuisines"
       />
       <select
         value={category}
