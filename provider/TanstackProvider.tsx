90383018f2a274be66edb8f7c8b820320cd2ab6f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-24 16:48:53 +0900

    updaete

---
+++ provider/TanstackProvider.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { type ReactNode } from 'react';
+
+type TanstackProviderProps = {
+  children: ReactNode;
+};
+
+const TanstackProvider = ({ children }: TanstackProviderProps) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
