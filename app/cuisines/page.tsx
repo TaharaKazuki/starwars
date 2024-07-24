3e28474b0aac9b86762b65abd4837d2a40bd3b68
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-24 19:42:36 +0900

    update

--- app/cuisines/page.tsx
+++ app/cuisines/page.tsx
@@ -20,7 +20,7 @@ const CuisinesPage = () => {
   return (
     <div className="flex flex-col gap-2">
       <div>
-        <h1 className="text-4xl font-bold">Products</h1>
+        <h1 className="text-4xl font-bold">Cuisines</h1>
       </div>
       <CuisinesListFilter
         onChange={(filters) => {
